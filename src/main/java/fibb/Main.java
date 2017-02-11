5fce15dd1c8bc702831e94bd9ee793f2473f7ae4
Tomasz Śmiechowicz <tomasz.smiechowicz@hycom.pl>
2017-02-11 11:16:16 +0100

    za pomoca http handlera

--- src/main/java/fibb/Main.java
+++ src/main/java/fibb/Main.java
@@ -1,18 +1,24 @@
 package fibb;
 
+import ratpack.exec.Promise;
+import ratpack.http.client.HttpClient;
+import ratpack.http.client.ReceivedResponse;
 import ratpack.server.RatpackServer;
 
 import java.net.InetAddress;
+import java.net.URI;
+import java.time.Duration;
 
 /**
  * @author Tomasz Smiechowicz
  */
 public class Main {
     public static void main(String... args) throws Exception {
+        HttpClient httpClient = HttpClient.of(httpClientSpec -> httpClientSpec.readTimeout(Duration.ofMinutes(2)));
         RatpackServer.start(ratpackServerSpec -> ratpackServerSpec
                 .serverConfig(serverConfigBuilder -> serverConfigBuilder
                         .development(false)
-                        .port(8080)
+                        .port(5050)
                         .address(InetAddress.getByName("0.0.0.0"))//dlaczego tu jest tak - on powiedzial
                         .threads(4))
                 .handlers(
@@ -20,8 +26,19 @@ public class Main {
                             .prefix("fibb",
                                 fibb -> fibb
                                         .get(":value", ctx -> {
-                                            final String val = ctx.getAllPathTokens().get("value");
-                                            ctx.render(String.valueOf(generate(Integer.valueOf(val))));
+                                            final Long n = Long.parseLong(ctx.getAllPathTokens().get("value"));
+                                            if(n < 3){
+                                                ctx.render("1");
+                                            }
+                                            else {
+                                                final Promise<ReceivedResponse> receivedResponsePromiseFibbN_1 = httpClient.get(new URI("http://localhost:5050/fibb/" + (n - 1)));
+                                                final Promise<Long> promisedFibbN_1 = receivedResponsePromiseFibbN_1.map(bodyResponse -> Long.parseLong(bodyResponse.getBody()
+                                                                                                                                                                .getText()));
+                                                final Promise<ReceivedResponse> receivedResponsePromiseFibbN_2 = httpClient.get(new URI("http://localhost:5050/fibb/" + (n - 2)));
+                                                final Promise<Long> promisedFibbN_2 = receivedResponsePromiseFibbN_2.map(bodyResponse -> Long.parseLong(bodyResponse.getBody()
+                                                                                                                                                                .getText()));
+                                                promisedFibbN_1.then(fibbN_1 -> promisedFibbN_2.then(fibbN_2 -> ctx.render(String.valueOf(fibbN_1 + fibbN_2))));
+                                            }
                                         }))
                             .prefix("hello",
                                 hello -> hello
